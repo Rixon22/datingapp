ad0a9185778622b4b847e6f4b942b2c241b03f46
Rixon22 <roys0421@gmail.com>
2026-03-12 19:40:00 -0600

    [MOD] implementar paginación en el servicio de miembros y actualizar el controlador y repositorio para manejar solicitudes paginadas

--- client/src/core/services/members-service.ts
+++ client/src/core/services/members-service.ts
@@ -1,9 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams  } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { EditableMember, Member, Photo } from '../../types/member';
 import { Observable, tap } from 'rxjs';
 import { signal } from '@angular/core';
+import { PaginationResult } from '../../types/paginationMetadata';
 
 @Injectable({
   providedIn: 'root'
@@ -22,8 +23,12 @@ export class MembersService {
     );
   }
 
-  getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + "members");
+    getMembers(pageNumber = 1, pageSize = 5): Observable<PaginationResult<Member>> {
+    let params = new HttpParams();
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+
+    return this.http.get<PaginationResult<Member>>(this.baseUrl + "members", { params });
   }
 
   getPhotos(id: string) {

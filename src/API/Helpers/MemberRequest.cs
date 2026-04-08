9ae4354ee0f5308a3fba7412f9d3d0e996ca183c
Rogelio Seañez <102344406+Rixon22@users.noreply.github.com>
2026-04-08 16:49:19 -0600

    Parcial05 (#3)
    * [MOD] actualizar formulario de registro para usar Reactive Forms y agregar validaciones
    
    * [MOD] refactor registro para usar componente de entrada de texto y agregar validaciones de contraseña
    
    * [MOD] implementar registro de usuario en pasos con validaciones y campos adicionales
    
    * [MOD] agregar manejo de errores de validación en el registro de usuario y actualizar el formulario para incluir campos adicionales
    
    * [MOD] implementar paginación en el servicio de miembros y actualizar el controlador y repositorio para manejar solicitudes paginadas
    
    * [MOD] agregar soporte para filtrado de miembros por género y miembro actual en la API de miembros
    
    * agregar filtrado por edad en la API de miembros y actualizar pruebas de integración para género
    
    * [MOD] implementar modal de filtrado y actualizar servicio de miembros para soportar parámetros de filtrado
    
    * [MOD] actualizar especificaciones de la API para incluir parámetros de filtrado por edad y paginación
    
    * [MOD] agregar filtros por género y edad en el modal de filtrado, actualizar lógica de envío y restablecimiento de filtros
    
    * [MOD] agregar soporte para ordenar miembros por fecha de creación, última actividad y edad en el servicio y la API, actualizar modal de filtrado y pruebas de integración
    
    * [MOD] agregar lógica para almacenar y recuperar filtros de miembros en localStorage, implementar pipe para mostrar tiempo transcurrido
    
    * [MOD] mejorar el interceptor de carga para usar claves de caché basadas en parámetros de solicitud, actualizar el modal de filtrado para usar métodos de miembroParams y restablecer filtros en la lista de miembros
    
    * [MOD] agregar entidad MemberLike y lógica para gestionar relaciones de "me gusta" entre miembros, actualizar contexto de base de datos y migraciones
    
    * [MOD] agregar repositorio LikesRepository e interfaz ILikesRepository para gestionar relaciones de "me gusta" entre miembros
    
    * [MOD] actualizar pruebas de integración para utilizar variables de entorno en lugar de valores codificados, mejorar la gestión de tokens de autenticación y simplificar la configuración de encabezados
    
    * Agregar LikesService y lógica para gestionar relaciones de "me gusta", actualizar AccountService e InitService, y crear pruebas para LikesService
    
    * Implementar paginación en el servicio de Likes, actualizar el controlador y repositorio para manejar solicitudes paginadas, y agregar la clase LikesRequest para gestionar parámetros de consulta.

---
+++ src/API/Helpers/MemberRequest.cs
@@ -0,0 +1,11 @@
+namespace API.Helpers;
+
+public class MemberRequest : PaginationRequest
+{
+    public string? Gender { get; set; }
+    public string? CurrentMemberId { get; set; }
+    public int MinAge { get; set; } = 18;
+    public int MaxAge { get; set; } = 120;
+    public string OrderBy { get; set; } = "age";
+    
+}

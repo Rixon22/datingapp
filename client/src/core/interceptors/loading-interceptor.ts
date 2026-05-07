3a66f5fea155543da47402330c1cfc8d49294964
Rixon22 <roys0421@gmail.com>
2026-05-07 08:14:40 -0600

    Agregar lógica para eliminar mensajes: implementar el método DeleteMessage en el controlador y repositorio, actualizar la interfaz de usuario para permitir la eliminación de mensajes, y ajustar la lógica de filtrado para mensajes eliminados.

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -30,6 +30,10 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
     if (req.method.includes('POST') && req.url.includes('/likes')) {
       invalidateCache('/likes');
     }
+
+    if (req.method.includes('POST') && req.url.includes('/messages')) {
+      invalidateCache('/messages');
+    }
     
     if (req.method === 'GET') {
       const cachedResponse = cache.get(cacheKey);
@@ -41,7 +45,7 @@ export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   busyService.busy();
 
   return next(req).pipe(
-    delay(2000),
+    delay(100),
 
     tap(response => {
       cache.set(cacheKey, response)
